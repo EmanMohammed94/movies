25585e5c01fbf5d1a79caa048aa3a90f71af4357
eman mohammed <emohammdd073@gmail.com>
2023-03-15 20:51:51 +0200

    .

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import {HashRouter } from 'react-router-dom';
 import App from './App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
     <HashRouter>
    <App/>
     </HashRouter>
